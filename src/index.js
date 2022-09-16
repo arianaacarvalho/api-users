c221de3648a00d65849ac223e183e99197c3a1a2
Ariana <arianaacarvalho27@gmail.com>
2022-09-16 15:09:35 -0300

    inclusão de arquivos de memória'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const users = require('./users/routes')
+const logger = require('./middlewares/logger')
 
 const app = express()
 
 app.use(express.json())
 app.use('/users', users)
+app.use('/logger', logger)
 
 app
   .listen(3000, '0.0.0.0', () =>{
--- src/users/routes.js
+++ src/users/routes.js
@@ -1,36 +1,108 @@
-const {Router} = require('express')
-const withAsyncErrorHandler = require('../middlewares/async-error.js')
+const { Router } = require('express')
+
+const withAsyncErrorHandler = require('../middlewares/async-error')
 
 const router = Router()
 
-//CRUD:
-//Create: endpoint que faz a criação de novos usuários
-//POST /users {...users}
-const createUser = async (req, res) => {
-  res.staus(201).header('Location', '/users/???').send({})
+const { UsersRepository } = require('./repository')
+
+const repository = UsersRepository()
+
+const NotFound = {
+  error: 'Not found',
+  message: 'Resource not found',
 }
+
+/*
+  CRUD de usuários
+  - C: create
+  - R: read (listar + detalhes)
+  - U: update
+  - D: delete
+*/
+
+// ************
+// ** create **
+// ************
+
+const createUser = async (req, res) => {
+  // e se não for um JSON de usuário válido ?
+  const user = req.body
+
+  const inserted = await repository.insert(user)
+  const location = `/api/users/${inserted.id}`
+  res.status(201).header('Location', location).send(inserted)
+}
+
 router.post('/', withAsyncErrorHandler(createUser))
 
-//Read: endpoint que faz a leitura e lista usuários
-//GET /users {users: [..users]}
-router.get('/', withAsyncErrorHandler(async (req, res) => {
-  res.status(200).send({users: []})
-}))
-//GET /users/:id {...user}
-router.get('/:id', withAsyncErrorHandler(async (req, res) => {
-  res.status(200).send({})
-}))
+// ************
+// ** update **
+// ************
 
-//Update: endpoint que faz a atualização de dados
-//PUT /users/:id {...updatableUser}
-router.put('/:id', withAsyncErrorHandler(async (req, res) => {
-  res.status(200).send({})
-}))
+const updateUser = async (req, res) => {
+  // e se for NaN ?
+  const id = parseInt(req.params.id)
 
-//Delete: endpoint que remove um usário
-//DELETE /users/:id ''
-router.delete('/:id', async (req, res) => {
+  // e se não for um JSON de usuário válido ?
+  const body = req.body
+
+  // repetido
+  const registered = await repository.get(id)
+  if (!registered) {
+    return res.status(404).send(NotFound)
+  }
+
+  const user = { ...body, id }
+  const updated = await repository.update(user)
+  res.status(200).send(updated)
+}
+
+router.put('/:id', withAsyncErrorHandler(updateUser))
+
+// ************
+// ** delete **
+// ************
+
+const deleteUser = async (req, res) => {
+  // e se for NaN ?
+  const id = parseInt(req.params.id)
+
+  // repetido
+  const registered = await repository.get(id)
+  if (!registered) {
+    return res.status(404).send(NotFound)
+  }
+
+  await repository.del(id)
   res.status(204).send()
-})
+}
+
+router.delete('/:id', withAsyncErrorHandler(deleteUser))
+
+// **********
+// ** read **
+// **********
+
+const listUsers = async (_req, res) =>
+  repository
+    .list()
+    .then(users => res.status(200).send({ users }))
+
+const getUser = async (req, res) => {
+  // e se for NaN
+  const id = parseInt(req.params.id)
+
+  // repetido
+  const user = await repository.get(id)
+  if (!user) {
+    return res.status(404).send(NotFound)
+  }
+
+  res.status(200).send(user)
+}
+
+router.get('/', withAsyncErrorHandler(listUsers))
+router.get('/:id', withAsyncErrorHandler(getUser))
 
 module.exports = router
